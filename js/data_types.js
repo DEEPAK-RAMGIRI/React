52d3cf4824fdf14ce82ccd7581b3d9de6ee022fe
RAMGIRI  DEEPAK KUMAR <142658497+DEEPAK-RAMGIRI@users.noreply.github.com>
2025-08-23 10:40:14 +0530

    Update and rename temp to data_types.js

---
+++ js/data_types.js
@@ -0,0 +1,20 @@
+
+
+console.log("Hello World");
+// document.writeln("hello world")
+
+a = 10;
+b = 10.10;
+c = "normal";
+console.log(a, b ,c);
+
+
+let a = 10;
+let b = 10.1;
+let c = "Using let";
+console.log(a, b ,c);
+
+var d = 10;
+var e = 20.2;
+var f = "Using var";
+console.log(d, e ,f);
